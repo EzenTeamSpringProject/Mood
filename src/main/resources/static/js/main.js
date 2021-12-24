da65ea20311ba0e4938e5ea4ddcd8929c7a6ef6c
Haram Jeong <niceharam@gmail.com>
2021-12-25 00:00:48 +0900

    dropdown menu

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,12 @@
+const dropDown = document.getElementById('dropDown');
+const dropDownBtn = document.querySelector('#profile button');
+
+function handleDropDown() {
+  if (dropDown.classList.value === 'hidden') {
+    dropDown.classList.remove('hidden');
+  } else {
+    dropDown.classList.add('hidden');
+  }
+}
+
+dropDownBtn.addEventListener('click', handleDropDown);
